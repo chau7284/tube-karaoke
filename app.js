f8ba088b613d1c42c17216dcf5743e135b9d7125
kingpes <kingpesflutter@gmail.com>
2021-02-19 00:29:26 +0700

    fix

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.get('/get', key, async (req, res) => {
         console.log("<<<<<- GET-VIDEO: >>>>> " + videoId);
         await db.find_song_by_id(videoId, song => {
             if (song != null) {
-                if (song.video.length > 0 && !utils.checkExpire(song.video)) {
+                if (song.video.length > 0 && !utils.checkExpire(song.video[0])) {
                     console.log("<<<<<- RETURN-CACHE: >>>>> " + videoId);
                     res.json(song);
                     res.end();
